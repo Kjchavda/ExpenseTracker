562017bf06826a61e9ecf444859626a770a49980
Kjchavda <kulanjaychavda@gmail.com>
2025-06-27 15:53:32 +0530

    Made the login and signup UI

---
+++ frontend/expense-tracker/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense"
+
+function App() {
+  
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Root />} />
+          <Route path='/login' exact element={<Login />} />
+          <Route path='/signup' exact element={<SignUp />} />
+          <Route path='/dashboard' exact element={<Home />} />
+          <Route path='/income' exact element={<Income />} />
+          <Route path='/expense' exact element={<Expense />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
+
+const Root = () => {
+  //check if token exists in localstorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  //Redirect to dashboard if yes and login if no
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
