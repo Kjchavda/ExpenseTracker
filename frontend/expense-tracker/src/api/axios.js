66740ca133d740f54e0e98789b196e2f2ad9bb75
Kjchavda <kulanjaychavda@gmail.com>
2025-08-17 19:21:40 +0530

    added the backend to axios

--- frontend/expense-tracker/src/api/axios.js
+++ frontend/expense-tracker/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000'
+  baseURL: import.meta.env.VITE_API_URL,
 });
-
 // Request interceptor
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem('access_token');
