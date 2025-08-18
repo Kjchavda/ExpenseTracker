10ff5807bbdf2d89c1c3f8bd6fd31ac3dde5dc04
Kjchavda <kulanjaychavda@gmail.com>
2025-08-18 10:47:59 +0530

    fixed emailvalidator function

--- frontend/expense-tracker/src/utils/helper.js
+++ frontend/expense-tracker/src/utils/helper.js
@@ -1,7 +1,7 @@
 import moment from "moment";
 
 export const validateEmail = (email) => {
-    const regex = /^[^s@]+@[^\s@]+\.[^\s@]+$/;
+    const regex =/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
 };
 
